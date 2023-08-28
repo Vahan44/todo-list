0c89ecc29bed4e4e699c2f731915f9f87972a574
Name Surname <vahan.balyan.44@gmail.com>
2023-08-29 00:38:38 +0400

    as

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 
-root.render(<div className = "ontainer">
+root.render(<div className = "container">
     <Header/>
     <CreatTasc/>
     <Todio/>
